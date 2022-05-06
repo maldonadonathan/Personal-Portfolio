92eef8f81a7d0f634ab8d1387a87a0fed032f7ed
xzthan <nmaldonado2725@gmail.com>
2022-05-05 23:19:30 -0700

    Image Count

---
+++ script.js
@@ -0,0 +1,32 @@
+/* 
+
+about.html - 3
+achivements.html - 3
+hobbies.html - 3
+plan.html 3
+
+documents.html - 2
+recommendations - 0
+resume.html - 6
+
+
+portfolio.html - 8
+    about me - 3
+    chronoshift - 3
+    flew - 3
+    end of dragons - 3
+    personal statement - 3
+    adhd - 3
+    boyle - 3
+    journal - 3
+
+index .html - 3
+
+
+TOTAL IMAGES = 47
+
+
+
+
+https://colorpalettes.net/color-palette-1884/
+*/
