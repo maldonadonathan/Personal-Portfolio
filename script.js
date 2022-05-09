79a5d927853b0469a6ee5fbac996545e024b5745
xzthan <73207591+xzthan@users.noreply.github.com>
2022-05-09 10:06:48 -0700

    Background + carousel

--- script.js
+++ script.js
@@ -39,3 +39,35 @@ TOTAL IMAGES = 47
 
 https://colorpalettes.net/color-palette-1884/
 */
+var pos = 0;
+carouselDemo(pos);
+
+function add(n) {
+    carouselDemo(pos += n);
+}
+
+function current(n) {
+    carouselDemo(pos = n);
+}
+
+
+function carouselDemo(n) {
+    var i;
+    var slide = document.getElementsByClassName("carouselChild");
+    var circles = document.getElementsByClassName("circle");
+    if (n > slide.length) {
+        pos = 1;
+    }
+    if (n < 1) {
+        pos = slide.length
+    }
+    for (i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
+    for (i = 0; i < circles.length; i++) {
+        circles[i].className = circles[i].className.replace(" on", "");
+    }
+    const slideStyle = "block";
+    slide[pos - 1].style.display = slideStyle;
+    circles[pos - 1].className += " on";
+} 
