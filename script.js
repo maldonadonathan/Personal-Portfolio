cba0b6a74a693c7b373508492f57995e9253783c
xzthan <nmaldonado2725@gmail.com>
2022-05-09 14:29:09 -0700

    Image links

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-/* 
-
-about.html - 3
-achivements.html - 3
-hobbies.html - 3
-plan.html 3
-
-documents.html - 2
-recommendations - 0
-resume.html - 6
-
-
-portfolio.html - 8
-    about me - 3
-    chronoshift - 3
-    flew - 3
-    end of dragons - 3
-    personal statement - 3
-    adhd - 3
-    boyle - 3
-    journal - 3
-
-index .html - 3
-
-
-TOTAL IMAGES = 47
-
-
-    light blue - #A7CFE8
-        blue accent - #A9D9E6
-
-    pinkish red - #EF6968
-            pink accent - #F5B3B3
-
-    dark blue - #232F5F
-        dark blue accent - #364588
-
-    yellow - #FEEA7F
-
-https://colorpalettes.net/color-palette-1884/
-*/
 var pos = 0;
 carouselDemo(pos);
 
@@ -70,4 +29,4 @@ function carouselDemo(n) {
     const slideStyle = "block";
     slide[pos - 1].style.display = slideStyle;
     circles[pos - 1].className += " on";
-} 
+}
